eaa87476ee2b0f72b90bdad0b91324cc51b41726
rusyamurzin <scretch235@mail.ru>
2019-05-12 20:31:16 +0300

    Arbitrary jump module

--- mythril/analysis/modules/arbitrary_jump.py
+++ mythril/analysis/modules/arbitrary_jump.py
@@ -20,7 +20,7 @@ class ArbitraryJumpModule(DetectionModule):
             swc_id=ARBITRARY_JUMP,
             description="Checks for usage assembly instructions.",
             entrypoint="callback",
-            pre_hooks=["ASSEMBLY"],
+            pre_hooks=["MSTORE"],
         )
 
     def execute(self, state: GlobalState) -> list:
@@ -39,41 +39,42 @@ def _analyze_state(state) -> list:
     :param state:
     :return:
     """
-    log.info("Exceptions module: found ASSERT_FAIL instruction")
-
-    log.debug("ASSERT_FAIL in function " + state.environment.active_function_name)
+    log.info("Arbitrary jump module: found MSTORE instruction")
 
     try:
-        address = state.get_current_instruction()["address"]
-
+        instruction = state.get_current_instruction()
+        address = instruction["address"]
         description_tail = (
-            "It is possible to trigger an exception (opcode 0xfe). "
-            "Exceptions can be caused by type errors, division by zero, "
-            "out-of-bounds array access, or assert violations. "
-            "Note that explicit `assert()` should only be used to check invariants. "
-            "Use `require()` for regular input checking."
+            "The use of assembly is error-prone and should be avoided."
+            "Usage MSTORE instruction or assign operator in assembly block"
+            "is able to point a function type variable to any code instruction."
         )
 
-        transaction_sequence = solver.get_transaction_sequence(
-            state, state.mstate.constraints
-        )
-        debug = json.dumps(transaction_sequence, indent=4)
+        #May be check for assembly in str(constraints) is reduntant
+        constraints = state.mstate.constraints
+        if "assembly" in str(constraints):
+            log.debug("ASSEMBLY usage in function " + state.environment.active_function_name)
+            transaction_sequence = solver.get_transaction_sequence(
+                state, state.mstate.constraints
+            )
+            debug = json.dumps(transaction_sequence, indent=4)
 
-        issue = Issue(
-            contract=state.environment.active_account.contract_name,
-            function_name=state.environment.active_function_name,
-            address=address,
-            swc_id=ASSERT_VIOLATION,
-            title="Exception State",
-            severity="Low",
-            description_head="A reachable exception has been detected.",
-            description_tail=description_tail,
-            bytecode=state.environment.code.bytecode,
-            debug=debug,
-            gas_used=(state.mstate.min_gas_used, state.mstate.max_gas_used),
-        )
-        return [issue]
-
+            issue = Issue(
+                contract=state.environment.active_account.contract_name,
+                function_name=state.environment.active_function_name,
+                address=address,
+                swc_id=ARBITRARY_JUMP,
+                title="ASSEMBLY usage",
+                severity="Low",
+                description_head="Arbitrary jump is possible.",
+                description_tail=description_tail,
+                bytecode=state.environment.code.bytecode,
+                debug=debug,
+                gas_used=(state.mstate.min_gas_used, state.mstate.max_gas_used),
+            )
+            return [issue]
+        else:
+            log.debug("ASSEMBLY usage not found in " + state.environment.active_function_name)
     except UnsatError:
         log.debug("no model found")
 
